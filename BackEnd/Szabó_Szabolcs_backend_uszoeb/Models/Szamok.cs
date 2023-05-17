6b7c5d3ece44a3f7edfcce977d6893290630314a
szaboszabolcs <szabosz@kkszki.hu>
2023-05-17 13:46:07 +0200

    Úszó eb feladat.

---
+++ BackEnd/Szabó_Szabolcs_backend_uszoeb/Models/Szamok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace Szabó_Szabolcs_backend_uszoeb.Models
+{
+    public partial class Szamok
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int Versenyzoid { get; set; }
+
+        [JsonIgnore]
+
+        public virtual Versenyzok Versenyzo { get; set; }
+    }
+}
